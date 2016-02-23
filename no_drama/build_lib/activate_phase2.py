23a6ef40b0ed048102a38629c09592d724202260
Ross Karchner <ross.karchner@cfpb.gov>
2016-02-23 18:15:58 -0500

    Switched from None comparison to truthiness check.

--- no_drama/build_lib/activate_phase2.py
+++ no_drama/build_lib/activate_phase2.py
@@ -2,7 +2,7 @@ import os
 
 from dfd_paths import root, update_symlink
 
-if update_symlink is not None:
+if update_symlink:
     if os.path.exists(update_symlink):
         os.remove(update_symlink)
     os.symlink(root, update_symlink)
