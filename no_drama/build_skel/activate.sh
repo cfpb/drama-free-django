59940715ffcb629f4b19a937bd38cd3bbd636a34
Ross M Karchner <ross@karchner.com>
2016-03-07 13:03:50 -0500

    Update activate.sh

--- no_drama/build_skel/activate.sh
+++ no_drama/build_skel/activate.sh
@@ -12,8 +12,8 @@ python -m virtualenv --no-pip --no-wheel --no-setuptools $DIR/empty_venv
 python -m virtualenv --extra-search-dir=bootstrap_wheels $DIR/venv
 
 SITE_PACKAGES=`find venv -name 'site-packages'`
-echo `python lib/dfd.py build_lib`>$SITE_PACKAGES/no_drama.pth
-echo `python lib/dfd.py django_root`>>$SITE_PACKAGES/no_drama.pth
+echo `$DIR/venv/bin/python $DIR/lib/dfd.py build_lib`>$SITE_PACKAGES/no_drama.pth
+echo `$DIR/venv/bin/python $DIR/lib/dfd.py django_root`>>$SITE_PACKAGES/no_drama.pth
 
 $DIR/venv/bin/wheel install wheels/*.whl --force
 $DIR/venv/bin/django-admin.py collectstatic --noinput
