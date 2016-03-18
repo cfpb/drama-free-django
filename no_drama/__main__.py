fa81fc337f7dddd7a8759648f5074db4f3182238
Ross Karchner <ross.karchner@cfpb.gov>
2016-03-18 17:30:30 -0400

    builds, when run as an executable, now support a --destination / -d arg...
    ... which lets you tell it where to deploy to.

--- no_drama/__main__.py
+++ no_drama/__main__.py
@@ -249,23 +249,33 @@ self_extraction_script = """
 import os
 import zipfile
 import subprocess
+import argparse
 
-prefix = '%s'
+def deploy_to(deployment_dir=None):
+    prefix = '%s'
 
-zip_path= os.path.join(os.getcwd(),__loader__.archive)
-destination = os.path.dirname(zip_path)
-bundle = zipfile.ZipFile(zip_path)
+    zip_path= os.path.join(os.getcwd(),__loader__.archive)
+    destination = deployment_dir or os.path.dirname(zip_path)
+    bundle = zipfile.ZipFile(zip_path)
 
-os.chdir(destination)
-for member in bundle.infolist():
-    filename = member.filename
-    if filename.startswith(prefix) and not filename.endswith('/'):
-        bundle.extract(member)
+    original_cwd = os.getcwd()
+    os.chdir(destination)
+    for member in bundle.infolist():
+        filename = member.filename
+        if filename.startswith(prefix) and not filename.endswith('/'):
+            bundle.extract(member)
 
-bundle_dir = os.path.join(destination,prefix)
-activate_script = os.path.join(bundle_dir, 'activate.sh')
+    bundle_dir = os.path.join(destination,prefix)
+    activate_script = os.path.join(bundle_dir,'activate.sh')
 
-subprocess.call(['sh',activate_script])
+    os.chdir(original_cwd)
+    subprocess.call(['sh',activate_script])
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="deploy this release")
+    parser.add_argument('--destination', '-d')
+    args = parser.parse_args()
+    deploy_to(args.destination)
 """
 
 if __name__ == '__main__':
