c9ee91c0b09aeaa508a71ff5e2eddb908eccab3b
Ross Karchner <ross.karchner@cfpb.gov>
2016-03-22 00:18:24 -0400

    factor some logic to logical modules

--- no_drama/__main__.py
+++ no_drama/__main__.py
@@ -1,202 +1,145 @@
 #!/usr/bin/env python
 
 import argparse
-import tempfile
 import os
 import os.path
 import stat
 import shutil
-import subprocess
 import zipfile
 import glob
 import fnmatch
 import json
-import hashlib
-import pip
 
-from itertools import chain
+from .context import temp_directory
+from .pip_automation import save_wheels
+from .executable import make_executable
 
 this_file = os.path.realpath(__file__)
 this_directory = os.path.dirname(this_file)
 build_skel = os.path.join(this_directory, 'build_skel')
 
 
-def expand_globs(globs_or_not):
-    return chain(*[glob.glob(x) for x in globs_or_not])
-
-
-def hash_for_path(path):
-    hasher = hashlib.sha1()
-    with open(path, 'rb') as afile:
-        buf = afile.read()
-        hasher.update(buf)
-    return hasher.hexdigest()
-
-def cache_marker_for_path(path):
-    req_hash = hash_for_path(path)
-    return 'requirements_hashes/%s' % req_hash
-
-def is_cache_update_required(path):
-    marker_path = cache_marker_for_path(path)
-    return not os.path.exists(marker_path)
-    
-def record_req_cached(path):
-    marker_path = cache_marker_for_path(path)
-    if not os.path.exists('requirements_hashes'):
-        os.mkdir('requirements_hashes')
-    with open(marker_path,'wb') as marker_file:
-        marker_file.write('')
-
-def save_wheels(destination, packages=[], requirements_paths=[]):
-    cache_wheel_command_prefix = "wheel --find-links=wheelhouse --wheel-dir=wheelhouse".split()
-    save_wheel_command_prefix =  (" wheel --find-links=wheelhouse --no-index --wheel-dir=%s" % destination).split()
-    
-    requirements_install_args =[]
-    requirements_cache_args = []
-    
-    record_caches =[]
-    for path in requirements_paths:
-        requirements_install_args += ['-r', path]
-        if is_cache_update_required(path):
-            requirements_cache_args += ['-r', path]
-            record_caches.append(path)
-
-    pip.main(cache_wheel_command_prefix + packages + requirements_cache_args)
-    pip.main(save_wheel_command_prefix + packages + requirements_install_args)
-    for path in record_caches:
-        record_req_cached(path)
-    
-
 def stage_bundle(cli_args):
-    archive_basename = "_%s_%s" % (cli_args.name, cli_args.label)
-    executable_path = os.path.join(os.getcwd(),archive_basename[1:]+'.zip')
-    
+    archive_basename = "%s_%s" % (cli_args.name, cli_args.label)
+    executable_path = os.path.join(os.getcwd(), archive_basename + '.zip')
+
     if os.path.exists(executable_path) and not cli_args.f:
-        print "%s already exists, skipping. Call with -f to force rebuild"
+        print "%s already exists, skipping. Call with -f to force rebuild" % executable_path
         return
 
-    staging_dir = tempfile.mkdtemp()
-    build_dir = os.path.join(staging_dir, cli_args.label)
+    with temp_directory() as staging_dir:
+        build_dir = os.path.join(staging_dir, cli_args.label)
 
-    shutil.copytree(build_skel, build_dir)
+        shutil.copytree(build_skel, build_dir)
 
-    # these are wheels needed during activation
-    bootstrap_wheels = ['virtualenv','pip','wheel','distribute']
-    bootstrap_wheels_destination = os.path.join(build_dir,'bootstrap_wheels')
-    save_wheels(packages=bootstrap_wheels, destination=bootstrap_wheels_destination)
+        # these are wheels needed during activation
+        bootstrap_wheels = ['virtualenv', 'pip', 'wheel', 'distribute']
+        bootstrap_wheels_destination = os.path.join(
+            build_dir, 'bootstrap_wheels')
+        save_wheels(packages=bootstrap_wheels,
+                    destination=bootstrap_wheels_destination)
 
-    # move just the wheels we want into the bundle dir  
-    wheel_destination = os.path.join(build_dir, 'wheels')
-    if cli_args.r:
-        save_wheels(destination= wheel_destination, requirements_paths = cli_args.r)
+        # move just the wheels we want into the bundle dir
+        wheel_destination = os.path.join(build_dir, 'wheels')
+        if cli_args.r:
+            save_wheels(
+                destination=wheel_destination,
+                requirements_paths=cli_args.r)
 
-    # copy django project into bundle dir
-    project_complete_path = os.path.join(os.getcwd(), cli_args.project_path)
-    project_norm_path = os.path.normpath(project_complete_path)
-    project_slug = os.path.basename(project_norm_path)
-    project_destination = os.path.join(build_dir, project_slug)
-    shutil.copytree(cli_args.project_path, project_destination)
+        # copy django project into bundle dir
+        project_complete_path = os.path.join(
+            os.getcwd(), cli_args.project_path)
+        project_norm_path = os.path.normpath(project_complete_path)
+        project_slug = os.path.basename(project_norm_path)
+        project_destination = os.path.join(build_dir, project_slug)
+        shutil.copytree(cli_args.project_path, project_destination)
 
-    # install paths.d/0_build.json, so activate.sh can find the django_root
-    paths_d = os.path.join(build_dir, 'paths.d')
-    initial_paths_path = os.path.join(paths_d, '0_build.json')
+        # install paths.d/0_build.json, so activate.sh can find the django_root
+        paths_d = os.path.join(build_dir, 'paths.d')
+        initial_paths_path = os.path.join(paths_d, '0_build.json')
 
-    with open(initial_paths_path, 'wb') as initial_paths_file:
-        json.dump({'django_root': project_slug}, initial_paths_file)
+        with open(initial_paths_path, 'wb') as initial_paths_file:
+            json.dump({'django_root': project_slug}, initial_paths_file)
 
-    if cli_args.static:
-        for index,path in enumerate(cli_args.static):
-            destination = os.path.join(build_dir, 'static.in/%s/' % index)
-            shutil.copytree(path,destination )
+        if cli_args.static:
+            for index, path in enumerate(cli_args.static):
+                destination = os.path.join(build_dir, 'static.in/%s/' % index)
+                shutil.copytree(path, destination)
 
-    if cli_args.aux:
-        aux_root = os.path.join(build_dir, 'aux')
-        for aux_spec in cli_args.aux:
-            if '=' in aux_spec:
-                slug, src = aux_spec.split('=')
-            else:
-                # normalize the path, so that it  will not end in a /
-                # required for basename to get the last path component
-                # would auth_path.split('/')[-1] be simpler? maybe.
-                norm_path = os.path.normpath(aux_spec)
-                slug = os.path.basename(norm_path)
-                src = aux_spec
+        if cli_args.aux:
+            aux_root = os.path.join(build_dir, 'aux')
+            for aux_spec in cli_args.aux:
+                if '=' in aux_spec:
+                    slug, src = aux_spec.split('=')
+                else:
+                    # normalize the path, so that it  will not end in a /
+                    # required for basename to get the last path component
+                    # would auth_path.split('/')[-1] be simpler? maybe.
+                    norm_path = os.path.normpath(aux_spec)
+                    slug = os.path.basename(norm_path)
+                    src = aux_spec
 
-            destination = os.path.join(aux_root, slug)
-            shutil.copytree(src, destination)
+                destination = os.path.join(aux_root, slug)
+                shutil.copytree(src, destination)
 
-    
-    archive_name = shutil.make_archive(archive_basename,
-                                       'zip',
-                                       root_dir=staging_dir,
-                                       base_dir=cli_args.label)
+        archive_name = shutil.make_archive(archive_basename,
+                                           'zip',
+                                           root_dir=staging_dir,
+                                           base_dir=cli_args.label)
 
-    build_zip = zipfile.ZipFile(archive_name, 'a')
-
-    build_zip.writestr('__main__.py', self_extraction_script % cli_args.label)
-    build_zip.close()
-
-    executable_preamble = "#!/usr/bin/env python2.7\n"
-
-    with open(executable_path, 'wb') as executable_file:
-        executable_file.write(executable_preamble)
-        executable_file.write(open(archive_name).read())
-
-    exe_permissions = os.stat(executable_path)[0]
-    new_permissions = exe_permissions | stat.S_IXOTH | stat.S_IXGRP | stat.S_IXUSR
-    os.chmod(executable_path, new_permissions)
-    os.unlink(archive_name)
-    print("generated build at %s" % executable_path)
-    shutil.rmtree(staging_dir)
-    print("deleted temporary files")
+        make_executable(archive_name, prefix=cli_args.label)
+        print("generated build at %s" % executable_path)
 
 
 def inject_configuration(cli_args):
-    staging_dir = tempfile.mkdtemp()
-    build_zip_path = os.path.join(staging_dir, 'build.zip')
+    with temp_directory() as staging_dir:
+        build_zip_path = os.path.join(staging_dir, 'build.zip')
 
-    shutil.copyfile(cli_args.build_zip, build_zip_path)
+        shutil.copyfile(cli_args.build_zip, build_zip_path)
 
-    build_zip = zipfile.ZipFile(build_zip_path, mode="a")
+        build_zip = zipfile.ZipFile(build_zip_path, mode="a")
 
-    # figuring out where things are in this zip file...
-    all_the_wheels = fnmatch.filter(build_zip.namelist(), '*/wheels/*.whl')
-    zip_wheel_dir = os.path.dirname(all_the_wheels[0])
-    zip_root = os.path.normpath(os.path.join(zip_wheel_dir,'../'))
+        # figuring out where things are in this zip file...
+        all_the_wheels = fnmatch.filter(build_zip.namelist(), '*/wheels/*.whl')
+        zip_wheel_dir = os.path.dirname(all_the_wheels[0])
+        zip_root = os.path.normpath(os.path.join(zip_wheel_dir, '../'))
 
-    # move just the wheels we want into the bundle dir
-    wheel_destination = os.path.join(staging_dir, 'wheels')
-    if cli_args.requirements_file:
-        save_wheels(requirements_file=cli_args.requirements_file,
-                    destination=wheel_destination)
-        wheel_pattern = os.path.join(staging_dir, 'wheels/*.whl')
+        # move just the wheels we want into the bundle dir
+        wheel_destination = os.path.join(staging_dir, 'wheels')
+        if cli_args.requirements_file:
+            save_wheels(requirements_file=cli_args.requirements_file,
+                        destination=wheel_destination)
+            wheel_pattern = os.path.join(staging_dir, 'wheels/*.whl')
 
-        saved_wheels = glob.glob(wheel_pattern)
+            saved_wheels = glob.glob(wheel_pattern)
 
-        for wheel_path in saved_wheels:
-            name = os.path.join(zip_wheel_dir, os.path.basename(wheel_path))
-            build_zip.write(wheel_path, arcname=name)
+            for wheel_path in saved_wheels:
+                name = os.path.join(
+                    zip_wheel_dir, os.path.basename(wheel_path))
+                build_zip.write(wheel_path, arcname=name)
 
-    build_zip.write(cli_args.vars, os.path.join(zip_root, 'environment.json'))
+        build_zip.write(
+            cli_args.vars, os.path.join(
+                zip_root, 'environment.json'))
 
-    if cli_args.paths:
-        build_zip.write(cli_args.paths, os.path.join(zip_root, 'paths.json'))
+        if cli_args.paths:
+            build_zip.write(
+                cli_args.paths, os.path.join(
+                    zip_root, 'paths.json'))
 
-    if cli_args.prepend_wsgi:
-        build_zip.write(cli_args.append_wsgi,
-                        os.path.join(zip_root, 'pre-wsgi.py-fragment'))
+        if cli_args.prepend_wsgi:
+            build_zip.write(cli_args.append_wsgi,
+                            os.path.join(zip_root, 'pre-wsgi.py-fragment'))
 
-    if cli_args.append_wsgi:
-        build_zip.write(cli_args.append_wsgi,
-                        os.path.join(zip_root, 'pre-wsgi.py-fragment'))
+        if cli_args.append_wsgi:
+            build_zip.write(cli_args.append_wsgi,
+                            os.path.join(zip_root, 'pre-wsgi.py-fragment'))
 
-    build_zip.close()
-    build_filename = os.path.basename(cli_args.build_zip)
-    name, ext = os.path.splitext(build_filename)
-    release_filename = "%s_%s%s" % (name, cli_args.slug, ext)
-    shutil.copyfile(build_zip_path, release_filename)
-    shutil.rmtree(staging_dir)
-    print("deleted temporary files")
+        build_zip.close()
+        build_filename = os.path.basename(cli_args.build_zip)
+        name, ext = os.path.splitext(build_filename)
+        release_filename = "%s_%s%s" % (name, cli_args.slug, ext)
+        shutil.copyfile(build_zip_path, release_filename)
 
 
 def main():
@@ -222,71 +165,48 @@ def main():
                               help='extra directories to include, in form'
                               ' name=/path/to/dir.')
 
-    build_parser.add_argument('--static', action='append',
-                              help='extra directories to include in static files'
-                              'to be served from STATIC_ROOT')
-                              
-    build_parser.add_argument('-f', action='store_true', help="ignore if this build already exists")
-    
+    build_parser.add_argument(
+        '--static',
+        action='append',
+        help='extra directories to include in static files'
+        'to be served from STATIC_ROOT')
+
+    build_parser.add_argument(
+        '-f',
+        action='store_true',
+        help="ignore if this build already exists")
+
     build_parser.set_defaults(func=stage_bundle)
 
     # release parser arguments
     release_parser.add_argument('build_zip', help='path to a zip file'
                                 " generated with 'no-drama build'")
-    release_parser.add_argument('vars', help="JSON dictionary of env variables")
+    release_parser.add_argument(
+        'vars', help="JSON dictionary of env variables")
     release_parser.add_argument('slug', help="a label for this release")
 
     release_parser.add_argument('--paths', help="json file for overriding"
                                 " default paths")
-    release_parser.add_argument('--requirements_file', help="just like you would"
-                        " 'pip install -r'. Let's you add more wheels to the build")
+    release_parser.add_argument(
+        '--requirements_file', help="just like you would"
+        " 'pip install -r'. Let's you add more wheels to the build")
 
-    release_parser.add_argument('--prepend-wsgi',
-                        help="text file w/ additional python code to modify "
-                             "the environment before the wsgi 'application' is"
-                             " created")
+    release_parser.add_argument(
+        '--prepend-wsgi',
+        help="text file w/ additional python code to modify "
+        "the environment before the wsgi 'application' is"
+        " created")
 
-    release_parser.add_argument('--append-wsgi',
-                        help="text file w/ additional python code to modify "
-                        "the wsgi 'application'")
+    release_parser.add_argument(
+        '--append-wsgi',
+        help="text file w/ additional python code to modify "
+        "the wsgi 'application'")
 
     release_parser.set_defaults(func=inject_configuration)
 
     args = parser.parse_args()
     args.func(args)
 
-self_extraction_script = """
-import os
-import zipfile
-import subprocess
-import argparse
-
-def deploy_to(deployment_dir=None):
-    prefix = '%s'
-
-    zip_path= os.path.join(os.getcwd(),__loader__.archive)
-    destination = deployment_dir or os.path.dirname(zip_path)
-    bundle = zipfile.ZipFile(zip_path)
-
-    original_cwd = os.getcwd()
-    os.chdir(destination)
-    for member in bundle.infolist():
-        filename = member.filename
-        if filename.startswith(prefix) and not filename.endswith('/'):
-            bundle.extract(member)
-
-    bundle_dir = os.path.join(destination,prefix)
-    activate_script = os.path.join(bundle_dir,'activate.sh')
-
-    os.chdir(original_cwd)
-    subprocess.call(['sh',activate_script])
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="deploy this release")
-    parser.add_argument('--destination', '-d')
-    args = parser.parse_args()
-    deploy_to(args.destination)
-"""
 
 if __name__ == '__main__':
     main()
